f4fd5431c0c78d9487eefbadc4952e4301b547bf
bmi921 <bmi921gm@gmail.com>
2025-05-17 17:52:47 +0900

    delete: dotenvで環境変数を取得するのをやめた

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import axios from "axios";
 import cors from "cors";
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
